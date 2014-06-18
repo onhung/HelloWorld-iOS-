5aad62137a531c45de67afaa8dcaa02ed546cc64
Hung On <hungon@Hungs-MacBook-Pro.local>
2014-06-17 21:52:48 -0700

    Hello World iOS
    Initial iOS tutorial application for iOS iPhone platform

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Hung  On on 6/17/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld/HelloWorldViewController.h
@@ -0,0 +1,15 @@
+//
+//  HelloWorldViewController.h
+//  HelloWorld
+//
+//  Created by Hung  On on 6/17/14.
+//  Copyright (c) 2014 Hung  On. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController
+
+-(IBAction)showMessage;
+
+@end
